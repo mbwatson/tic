1d6601494ddb5bd1c493114eb8b509718dc3df6b
Matt Watson <mattwat@gmail.com>
2020-09-24 09:55:21 -0400

    change redirect to dev-ctmd

--- api/app.js
+++ api/app.js
@@ -59,5 +59,5 @@ app.use('/graphics', require('./routes/graphics'))
 // Auth
 app.post('/auth', (req, res) => {
   const code = req.body.code
-  res.redirect(`https://auth-fuse.renci.org/v1/authorize?apikey=${ AUTH_API_KEY }&provider=venderbilt&return_url=http://ctmd.renci.org/api/auth/&code=${ code }`)
+  res.redirect(`https://auth-fuse.renci.org/v1/authorize?apikey=${ AUTH_API_KEY }&provider=venderbilt&return_url=https://dev-ctmd.renci.org/&code=${ code }`)
 })
