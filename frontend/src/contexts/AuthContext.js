7355df2bf2c20e10334492b55be96f109b25882e
Matt Watson <mattwat@gmail.com>
2020-09-25 08:08:08 -0400

    reduce referrer substring compare

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -15,7 +15,7 @@ const emptyUser = {
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState()
     const [localStorageUser, setLocalStorageUser] = useLocalStorage('ctmd-user')
-    const validReferrer = document.referrer.includes('https://redcap.vanderbilt.edu/plugins/TIN') || process.env.NODE_ENV === 'development'
+    const validReferrer = document.referrer.includes('redcap.vanderbilt.edu') || process.env.NODE_ENV === 'development'
 
     const logout = () => {
         localStorage.removeItem('ctmd-user')
